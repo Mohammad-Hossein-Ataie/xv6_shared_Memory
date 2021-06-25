e00413db83e2a33d21fd462ac7a1c11e80182d28
MHAtaie <mh.ataie@ut.ac.ir>
2021-06-25 04:06:15 -0700

    Scheduler change to just round robin for project 5

--- proc.c
+++ proc.c
@@ -7,7 +7,7 @@
 #include "proc.h"
 #include "spinlock.h"
 #include "reentrantlock.h"
-
+int scheduler_algorithm = ROUND_ROBIN;
 struct {
   struct spinlock lock;
   struct proc proc[NPROC];
@@ -323,36 +323,45 @@ wait(void)
 void
 scheduler(void)
 {
-  struct proc *p;
-  struct cpu *c = mycpu();
-  c->proc = 0;
-  
-  for(;;){
-    // Enable interrupts on this processor.
-    sti();
+    if(scheduler_algorithm == ROUND_ROBIN)
+  {
+    roundRobinSched();
+  }
+  else
+  {
+    struct proc *p;  
+    struct cpu *c = mycpu();
+    c->proc = 0;
+    for(;;){
+      // Enable interrupts on this processor.
+      sti();
+      // Loop over process table looking for process to run.
+      acquire(&ptable.lock);
+      p = lotterySched();
 
-    // Loop over process table looking for process to run.
-    acquire(&ptable.lock);
-    for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
-      if(p->state != RUNNABLE)
-        continue;
+      if(p == 0)
+        p = FCFSSched();
+      if(p == 0)
+        p = prioritySched();
+      if(p !=0 ) {
+        // Switch to chosen process.  It is the process's job
+        // to release ptable.lock and then reacquire it
+        // before jumping back to us.
+        c->proc = p;
+        switchuvm(p);
+        p->state = RUNNING;
 
-      // Switch to chosen process.  It is the process's job
-      // to release ptable.lock and then reacquire it
-      // before jumping back to us.
-      c->proc = p;
-      switchuvm(p);
-      p->state = RUNNING;
+        swtch(&(c->scheduler), p->context);
+        switchkvm();
 
-      swtch(&(c->scheduler), p->context);
-      switchkvm();
+        // Process is done running for now.
+        // It should have changed its p->state before coming back.
+        c->proc = 0;
+      }
 
-      // Process is done running for now.
-      // It should have changed its p->state before coming back.
-      c->proc = 0;
+
+      release(&ptable.lock);
     }
-    release(&ptable.lock);
-
   }
 }
 
